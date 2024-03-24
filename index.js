5851581232a74d353824148a2ff0cf2ee1d71277
Collins Ngetich <collinsngetichk@gmail.com>
2024-03-24 14:55:51 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+  name: "Collins Ngetich",
+  age: 20,
+  department: "Dev",
+};
+updateEmployeeWithKeyAndValue(emplloyee, "position", "Manager");
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const clone = { ...employee };
+  delete clone[key];
+  return clone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
